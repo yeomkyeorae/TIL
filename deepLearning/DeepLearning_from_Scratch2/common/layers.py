46e8e18c4d4338cc0707027df8bbdd656feb74fc
yeomkyeorae <duarufp06@naver.com>
2020-03-13 21:27:47 +0900

    200313 | add two_layer_net

--- deepLearning/DeepLearning_from_Scratch2/common/layers.py
+++ deepLearning/DeepLearning_from_Scratch2/common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 
 class MatMul:
@@ -65,13 +66,21 @@ class Affine:
         return dx
 
 
-class Softmax:
-    def __init__(self):
-        self.params, self.grads = [], []
-        self.out = None
-
-    def forward(self, x):
-        self.out = softmax(x)
+# class Softmax:
+#     def __init__(self):
+#         self.params, self.grads = [], []
+#         self.out = None
+#
+#     def forward(self, x):
+#         self.out = softmax(x)
+#         return self.out
+#
+#     def backward(self, dout):
+#         dx = self.out * dout
+#         sumdx = np.sum(dx, axis=1, keepdims=True)
+#         dx -= self.out * sumdx
+#
+#         return dx
 
 
 class SoftmaxWithLoss:
@@ -84,3 +93,19 @@ class SoftmaxWithLoss:
         self.t = t
         self.y = softmax(x)
 
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
