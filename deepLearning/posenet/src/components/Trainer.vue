0c8235685419ca87a97ffc704e35f05a3012bfcd
yeomkyeorae <duarufp06@gmail.com>
2020-04-19 17:29:29 +0900

    200419 | add video by diffent method

--- deepLearning/posenet/src/components/Trainer.vue
+++ deepLearning/posenet/src/components/Trainer.vue
@@ -1,43 +1,63 @@
 <template>
 	<div id="theater">
-		<video id="video" src="http://upload.wikimedia.org/wikipedia/commons/7/79/Big_Buck_Bunny_small.ogv"
-			controls="false"></video>
 		<canvas id="canvas"></canvas>
-	</div>
-</template>
+		<video id="video" controls autoplay>
+			<source :src=videoSrc type=video/mp4> 
+		</video> 
+	</div> 
+</template> 
 
 <script>
 	export default {
-		name: 'Trainer',
+	name: 'Trainer',
 		data() {
 			return {
-				canvas: null,
-				ctx: null,
-				video: null
+				videoSrc: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+			}
+		},
+		methods: {
+			draw(v, c, w, h) {
+				if(v.paused || v.ended) return false;
+				c.drawImage(v,0,0,w,h);
+				setTimeout(this.draw, 20, v, c, w, h);
+			},
+			setup() {
+				document.addEventListener('DOMContentLoaded', function(){
+					var v = document.getElementById('video');
+					var canvas = document.getElementById('canvas');
+					var context = canvas.getContext('2d');
+
+					var cw = Math.floor(canvas.clientWidth / 100);
+					var ch = Math.floor(canvas.clientHeight / 100);
+					canvas.width = cw;
+					canvas.height = ch;
+
+					v.addEventListener('play', function(){
+						this.draw(this,context,cw,ch);
+					}, false);
+
+				},false);
 			}
 		},
 		mounted() {
-			this.canvas = document.getElementById('canvas');
-			this.ctx = this.canvas.getContext('2d');
-			this.video = document.getElementById('video');
 
-			this.video.addEventListener('loadedmetadata', function () {
-				this.canvas.width = this.video.videoWidth;
-				this.canvas.height = this.video.videoHeight;
-			});
-			this.video.addEventListener('play', function () {
-				var $this = this; //cache
-				(function loop() {
-					if (!$this.paused && !$this.ended) {
-						this.ctx.drawImage($this, 0, 0);
-						setTimeout(loop, 1000 / 30); // drawing at 30fps
-					}
-				})();
-			}, 0);
 		}
 	}
 </script>
-
 <style>
-
+	/* #canvas {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		width: 100%;
+		height: 100%;
+	}
+	#video {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin: -180px 0 0 -240px;
+	} */
 </style>
