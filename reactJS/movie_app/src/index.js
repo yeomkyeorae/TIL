45b9ddc677f1ad2e0cc7bd665ede6f82c8c19385
yeomkyeorae <duarufp06@naver.com>
2021-05-21 17:21:02 +0900

    reactJS: subtree studyingReact repo

--- reactJS/movie_app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-ReactDOM.render(
-  <App />,  // JSX (?) unique thing in React
-  document.getElementById('root')
-);
