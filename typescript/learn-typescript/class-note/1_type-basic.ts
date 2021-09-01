f7945a50d7c714c8f23e2cae55cdcef6dee61e5c
yeomkyeorae <duarufp06@naver.com>
2021-09-01 23:48:22 +0900

    typescript: basic type string, number, array

---
+++ typescript/learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,14 @@
+// TS 문자열 선언
+let str: string = "hello";
+
+// TS 숫자 선언
+let num: number = 100;
+
+// TS 문자열<숫자> 선언
+let arr: Array<number> = [10, 20, 30];
+
+// TS 문자열<문자> 선언
+let strArr: Array<string> = ["lion", "tiger"];
+
+// TS 문자열<숫자> - 다른 방식
+let items: number[] = [10, 20, 30];
