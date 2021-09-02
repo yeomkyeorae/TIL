ef5df2a8c477a3a6c0b427976415f6ad6a56230c
yeomkyeorae <duarufp06@naver.com>
2021-09-02 23:24:49 +0900

    typescript: basic type, function

--- typescript/learn-typescript/class-note/1_type-basic.ts
+++ typescript/learn-typescript/class-note/1_type-basic.ts
@@ -12,3 +12,21 @@ let strArr: Array<string> = ["lion", "tiger"];
 
 // TS 문자열<숫자> - 다른 방식
 let items: number[] = [10, 20, 30];
+
+// TS 튜블(배열의 index 별로 타입을 지정)
+let address: [string, number] = ["sejong", 165];
+
+// TS 객체
+let obj: object = {};
+let person: object = {
+  name: "yeom",
+  age: 21
+};
+// TS 객체 내부 property 타입 지정
+let man: { name: string; age: number } = {
+  name: "han",
+  age: 31
+};
+
+// TS boolean
+let isAvailable: boolean = true;
