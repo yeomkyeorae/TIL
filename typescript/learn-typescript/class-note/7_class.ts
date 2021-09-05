08bf6bd3ca27d08bb73b557b32d7f2f4e0ef8180
yeomkyeorae <duarufp06@naver.com>
2021-09-05 13:16:32 +0900

    typescript: class

---
+++ typescript/learn-typescript/class-note/7_class.ts
@@ -0,0 +1,10 @@
+class Tiger {
+    private name: string;
+    public age: number;
+    readonly log: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
