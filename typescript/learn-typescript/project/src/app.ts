605dd737ce3ea1decfebdb4298e4705ed32a82d4
yeomkyeorae <duarufp06@naver.com>
2022-02-24 22:47:06 +0900

    typescript: project, define type of chart library

--- typescript/learn-typescript/project/src/app.ts
+++ typescript/learn-typescript/project/src/app.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { Chart } from 'chart.js';
+import Chart from 'chart.js';
 // 타입 모듈
 import {
   CovidSummaryResponse,
@@ -45,10 +45,9 @@ function createSpinnerElement(id: string) {
 
 // state
 let isDeathLoading = false;
-const isRecoveredLoading = false;
 
 // api
-function fetchCovidSummary(): Promise<AxiosResponse<ConvidSummaryResponse>> {
+function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
   const url = 'https://api.covid19api.com/summary';
   return axios.get(url);
 }
@@ -191,8 +190,9 @@ async function setupData() {
   setLastUpdatedTimestamp(data);
 }
 
-function renderChart(data: any, labels: any) {
-  const ctx = $('#lineChart').getContext('2d');
+function renderChart(data: number[], labels: string[]) {
+  const lineChart = $('#lineChart') as HTMLCanvasElement;
+  const ctx = lineChart.getContext('2d');
   Chart.defaults.color = '#f5eaea';
   Chart.defaults.font.family = 'Exo 2';
   new Chart(ctx, {
@@ -212,11 +212,13 @@ function renderChart(data: any, labels: any) {
   });
 }
 
-function setChartData(data: any) {
-  const chartData = data.slice(-14).map((value: any) => value.Cases);
+function setChartData(data: CountrySummaryResponse) {
+  const chartData = data
+    .slice(-14)
+    .map((value: CountrySummaryInfo) => value.Cases);
   const chartLabel = data
     .slice(-14)
-    .map((value: any) =>
+    .map((value: CountrySummaryInfo) =>
       new Date(value.Date).toLocaleDateString().slice(5, -1)
     );
   renderChart(chartData, chartLabel);
